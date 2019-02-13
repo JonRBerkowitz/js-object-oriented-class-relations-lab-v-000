346dfe3219643ce36b6250a810fe8bf57b2741f0
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-13 22:34:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+let driverId = 0;
+
+class Driver {
+  constructor(id, name) {
+    this.id = driverId++;
+    this.name = name;
+  }
+}
+
+let passengerId = 0;
+
+class Passenger {
+  constructor(id, name) {
+    this.id = passengerId++;
+    this.name = name;
+  }
+}
+
+let tripId = 0;
+
+class {
+  constructor(id, driverId, passengerId) {
+    this.id = tripId++;
+  }
+}
