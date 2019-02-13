155da4a6b1892d5154e15aae028d855fd7708bee
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-13 23:19:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ class Driver {
   }
 
   trips() {
-    return store.trips;
+    return store.trips.filter(function(trip) {
+      return trip.driverId === this.id;
+    }.bind(this)
+  );
   }
 }
 
